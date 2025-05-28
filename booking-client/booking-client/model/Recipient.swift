d32184386b2e1c36a20119c9435439aa08cd08b9
Lucas Kemmler <lucas.kemmler.guitar@gmail.com>
2025-05-28 21:26:51 +0200

    Recipient class added

---
+++ booking-client/booking-client/model/Recipient.swift
@@ -0,0 +1,24 @@
+//
+//  Recipient.swift
+//  booking-client
+//
+//  Created by Lucas Kemmler Privat on 28.05.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Recipient {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var emailAdress: String
+    var salutation: String
+    
+    init(id: UUID, name: String, emailAdress: String, salutation: String) {
+        self.id = id
+        self.name = name
+        self.emailAdress = emailAdress
+        self.salutation = salutation
+    }
+}
