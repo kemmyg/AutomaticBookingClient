e2e14ca0f50c30fb9ae8132794f9158bbb501b94
Lucas Kemmler <lucas.kemmler.guitar@gmail.com>
2025-05-28 21:23:14 +0200

    Inital project creation

---
+++ booking-client/booking-client/booking_clientApp.swift
@@ -0,0 +1,32 @@
+//
+//  booking_clientApp.swift
+//  booking-client
+//
+//  Created by Lucas Kemmler Privat on 28.05.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct booking_clientApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ booking-client/booking-clientTests/booking_clientTests.swift
@@ -0,0 +1,16 @@
+//
+//  booking_clientTests.swift
+//  booking-clientTests
+//
+//  Created by Lucas Kemmler Privat on 28.05.25.
+//
+
+import Testing
+
+struct booking_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
